2fa210cee134c583b48a75f07ecfb7d8d6b27ddf
wodelu <1058744843@qq.com>
2019-03-31 19:43:16 +0800

    校区概况

--- app/routes/campus-overview/ksf-diag.js
+++ app/routes/campus-overview/ksf-diag.js
@@ -1,5 +1,5 @@
 import Route from '@ember/routing/route';
-// import { A } from '@ember/array';
+import { A } from '@ember/array';
 
 export default Route.extend({
     // model() {
@@ -11,4 +11,45 @@ export default Route.extend({
     //         ]),
     //     }
     // }
+    model() {
+        return {
+            iconInfo: '../../images/icon_info-s@3x.png',
+            business: A([{
+                rate: 50,
+                name: '消课率',
+                circleColor: A(['#3172E0', '#F4F5F7']),
+                circleData:A([
+                    { value: 0.5, name: '已消课' },
+                    { value: 0.5, name: '未消课' }
+                ])
+            },{
+                rate: 10,
+                name: '续费率',
+                circleColor: A(['#FFAB00', '#F4F5F7']),
+                circleData:A([
+                    { value: 0.1, name: '已续费' },
+                    { value: 0.9, name: '未续费' }
+                ])
+            }]),
+            behavior: A([{
+                rate: 20,
+                name: '覆盖率',
+                unit: '%',
+            },{
+                rate: 100,
+                name: '反馈次数',
+                unit: '次'
+            },{
+                rate: 20,
+                name: '及时率',
+                unit: '%',
+                icon: '../../images/icon_info-s@3x.png',
+            },{
+                rate: 20,
+                name: '准确率',
+                unit: '%'
+            }
+            ]),
+        }
+    }
 });
