48b92eb14151cb0c0cd9ae15a8ebbe59f1abe751
wodelu <1058744843@qq.com>
2019-03-31 16:47:52 +0800

    样式

--- app/routes/campus-overview/ksf-diag/educationmaster.js
+++ app/routes/campus-overview/ksf-diag/educationmaster.js
@@ -9,24 +9,24 @@ export default Route.extend({
                 name: '消课率',
                 circleColor: A(['#3172E0', '#F4F5F7']),
                 circleData:A([
-                    { value: 0.5, name: '已分配' },
-                    { value: 0.5, name: '未分配' }
+                    { value: 0.5, name: '已消课' },
+                    { value: 0.5, name: '未消课' }
                 ])
             },{
                 rate: 10,
                 name: '续费率',
                 circleColor: A(['#FFAB00', '#F4F5F7']),
                 circleData:A([
-                    { value: 0.1, name: '已分配' },
-                    { value: 0.9, name: '未分配' }
+                    { value: 0.1, name: '已续费' },
+                    { value: 0.9, name: '未续费' }
                 ])
             },{
                 rate: 25,
                 name: '签单率',
                 circleColor: A(['#57D9A3', '#F4F5F7']),
                 circleData:A([
-                    { value: 0.25, name: '已分配' },
-                    { value: 0.75, name: '未分配' }
+                    { value: 0.25, name: '已签单' },
+                    { value: 0.75, name: '未签单' }
                 ])
             }]),
             behavior: A([{
