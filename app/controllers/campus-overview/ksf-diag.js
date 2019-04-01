fc5899c647cfa5b9331f20f73bf484ee117c6494
Chenghao Sun <601330984@qq.com>
2019-04-01 17:56:30 +0800

    合代码准备发布

--- app/controllers/campus-overview/ksf-diag.js
+++ app/controllers/campus-overview/ksf-diag.js
@@ -9,29 +9,29 @@ export default Controller.extend({
     memberList: computed('switchValue', function () {
         if (this.switchValue === 0) {
             let tempArr = A([
-                {id: '4', name: '藏德防', sex: 1, icon:'/images/img_app_00.png', contact: '18842569864', jobCode: 0},
-                {id: '5', name: '莎莎', sex: 0, icon:'/images/img_app_00.png', contact: '18842569865', jobCode: 1},
-                {id: '6', name: '姚非', sex: 0, icon:'/images/img_app_00.png', contact: '18842569866', jobCode: 2},
+                {id: '4', name: '藏德防', sex: 1, icon:'/images/avatar_female_01@1x.jpg', contact: '18842569864', jobCode: 0},
+                {id: '5', name: '莎莎', sex: 0, icon:'/images/avatar_female_02@1x.jpg', contact: '18842569865', jobCode: 1},
+                {id: '6', name: '姚非', sex: 0, icon:'/images/avatar_female_03@1x.jpg', contact: '18842569866', jobCode: 2},
             ])
             this.set("curSelectId", tempArr.firstObject.id)
             return tempArr;
         } else if (this.switchValue === 1) {
             let tempArr = A([
-                {id: '7', name: '撒旦法', sex: 1, icon:'/images/img_app_00.png', contact: '18842569864', jobCode: 3},
+                {id: '7', name: '李林', sex: 1, icon:'/images/avatar_female_04@1x.jpg', contact: '18842585664', jobCode: 3},
             ])
             this.set("curSelectId", tempArr.firstObject.id)
             return tempArr;
         } else if (this.switchValue === 2) {
             let tempArr = A([
-                {id: '9', name: '积分', sex: 1, icon:'/images/img_app_00.png', contact: '18842569864', jobCode: 5},
+                {id: '9', name: '张翩翩', sex: 1, icon:'/images/avatar_female_05@1x.jpg', contact: '18848569864', jobCode: 5},
             ])
             this.set("curSelectId", tempArr.firstObject.id)
             return tempArr;
         } else if (this.switchValue === 3) {
             let tempArr = A([
-                {id: '4', name: '藏德防', sex: 1, icon:'/images/img_app_00.png', contact: '18842569864', jobCode: 0},
-                {id: '5', name: '莎莎', sex: 0, icon:'/images/img_app_00.png', contact: '18842569865', jobCode: 1},
-                {id: '6', name: '姚非', sex: 0, icon:'/images/img_app_00.png', contact: '18842569866', jobCode: 2},
+                {id: '4', name: '藏德防', sex: 1, icon:'/images/img_app_00.jpg', contact: '18842569864', jobCode: 0},
+                {id: '5', name: '莎莎', sex: 0, icon:'/images/img_app_00.jpg', contact: '18842569865', jobCode: 1},
+                {id: '6', name: '姚非', sex: 0, icon:'/images/img_app_00.jpg', contact: '18842569866', jobCode: 2},
             ])
             this.set("curSelectId", tempArr.firstObject.id)
             return tempArr;
