08d43fbb98ea9005a363f9620f6491193f449276
Chenghao Sun <601330984@qq.com>
2019-04-01 15:08:21 +0800

    数据管道页调整

--- app/controllers/data-platform/index.js
+++ app/controllers/data-platform/index.js
@@ -3,12 +3,12 @@ import { A } from '@ember/array';
 
 export default Controller.extend({
     fileList: A([
-        {id: '4', name: '文件名称再长一点', type: 'pdf', size: '李飞', time: '2019/08/08'},
-        {id: '5', name: '文件名称再长一点2', type: 'doc', size: '韩林', time: '2019/08/08'},
-        {id: '6', name: '文件名称再长一点3', type: 'excel', size: '戴晨晨', time: '2019/08/08'},
-        {id: '7', name: '文件名称再长一点4', type: 'unknown', size: '李飞', time: '2019/08/08'},
-        {id: '8', name: '文件名称再长一点5', type: 'unknown', size: '李飞', time: '2019/08/08'},
-        {id: '9', name: '文件名称再长一点6', type: 'unknown', size: '李飞', time: '2019/08/08'},
+        {id: '4', name: '校区发展规划', type: 'pdf', size: '李飞', time: '2019/02/08'},
+        {id: '5', name: '教师守则', type: 'doc', size: '韩林', time: '2019/02/05'},
+        {id: '6', name: '潜在客户2019年3月', type: 'excel', size: '戴晨晨', time: '2019/03/08'},
+        {id: '7', name: '测试文件', type: 'unknown', size: '韩林', time: '2019/02/18'},
+        {id: '8', name: '教室使用章程', type: 'doc', size: '李飞', time: '2019/01/22'},
+        {id: '9', name: '清明节放假通知', type: 'doc', size: '戴晨晨', time: '2019/03/25'},
     ]),
     appList: A([
         {id: '4', name: '咚哒SaaS', property: '平台', img: '/images/img_app_00.png', time: 1553933565274},
@@ -18,4 +18,10 @@ export default Controller.extend({
         {id: '5', name: '大众点评', property: '分类', img: '/images/img_api_00.png', time: 1553933565274},
         {id: '6', name: '小鹅通', property: '分类', img: '/images/img_api_01.png', time: 1553933565274},
     ]),
+
+    actions: {
+        xxOnClick() {
+            this.transitionToRoute("page-not-found");
+        },
+    }
 });
