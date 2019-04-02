0d4a45dda162192b2f7d783810b2b45d966cb9f8
Chenghao Sun <601330984@qq.com>
2019-04-02 10:39:36 +0800

    数据管道小修改

--- app/controllers/data-platform/index.js
+++ app/controllers/data-platform/index.js
@@ -3,12 +3,12 @@ import { A } from '@ember/array';
 
 export default Controller.extend({
     fileList: A([
-        {id: '4', name: '校区发展规划', type: 'pdf', size: '李飞', time: '2019/02/08'},
-        {id: '5', name: '教师守则', type: 'doc', size: '韩林', time: '2019/02/05'},
+        {id: '4', name: '2019年度百造朝阳校区发展规划', type: 'pdf', size: '李飞', time: '2019/02/08'},
+        {id: '5', name: '百造教师守则', type: 'doc', size: '韩林', time: '2019/02/05'},
         {id: '6', name: '潜在客户2019年3月', type: 'excel', size: '戴晨晨', time: '2019/03/08'},
-        {id: '7', name: '测试文件', type: 'unknown', size: '韩林', time: '2019/02/18'},
+        {id: '7', name: '教师评分考核表（2019.03）', type: 'excel', size: '韩林', time: '2019/04/02'},
         {id: '8', name: '教室使用章程', type: 'doc', size: '李飞', time: '2019/01/22'},
-        {id: '9', name: '清明节放假通知', type: 'doc', size: '戴晨晨', time: '2019/03/25'},
+        {id: '9', name: '清明节休假通知', type: 'doc', size: '戴晨晨', time: '2019/03/25'},
     ]),
     appList: A([
         {id: '4', name: '咚哒SaaS', property: '平台', img: '/images/img_app_00.png', time: 1553933565274},
